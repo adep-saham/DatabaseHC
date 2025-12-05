ea755f7db198a1fbc9041409269bfb58f6f4fb22
adep-saham <best.adeprasetyo@gmail.com>
2025-12-05 13:09:52 +0700

    Update audit_engine.py

--- audit_engine.py
+++ audit_engine.py
@@ -14,6 +14,7 @@ class AuditTrail:
             CREATE TABLE IF NOT EXISTS audit_log (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 action_time TEXT,
+                username TEXT,
                 user_role TEXT,
                 action_type TEXT,
                 employee_id TEXT,
@@ -26,21 +27,20 @@ class AuditTrail:
         conn.commit()
         conn.close()
 
-    def _write_db_log(self, action_type, user_role, employee_id, detail, before, after, ip):
+    def _write_db_log(self, action_time, username, user_role,
+                      action_type, employee_id, detail, before, after, ip):
+
         conn = sqlite3.connect(self.db_path)
         cur = conn.cursor()
 
         cur.execute("""
             INSERT INTO audit_log
-            (action_time, username, user_role, action_type, employee_id, detail,
-             before_data, after_data, ip_address)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            (action_time, username, user_role, action_type,
+             employee_id, detail, before_data, after_data, ip_address)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (
-            datetime.now().isoformat(timespec="seconds"),
-            user_role,
-            action_type,
-            employee_id,
-            detail,
+            action_time, username, user_role,
+            action_type, employee_id, detail,
             json.dumps(before, ensure_ascii=False),
             json.dumps(after, ensure_ascii=False),
             ip
@@ -49,14 +49,27 @@ class AuditTrail:
         conn.commit()
         conn.close()
 
-    def log_insert(self, user_role, employee_id, after, ip="0.0.0.0"):
-        self._write_db_log("INSERT", user_role, employee_id, "Insert employee", {}, after, ip)
+    # INSERT
+    def log_insert(self, username, user_role, employee_id, after, ip="0.0.0.0"):
+        action_time = datetime.now().isoformat(timespec="seconds")
+        self._write_db_log(
+            action_time, username, user_role,
+            "INSERT", employee_id, "Insert employee",
+            {}, after, ip
+        )
 
+    # UPDATE
     def log_update(self, username, user_role, employee_id, before, after, ip="0.0.0.0"):
+
+        action_time = datetime.now().isoformat(timespec="seconds")
+
         detail = json.dumps({
             k: {"before": before[k], "after": after[k]}
             for k in after if k in before and before[k] != after[k]
         }, ensure_ascii=False)
 
-        self._write_db_log("UPDATE", user_role, employee_id, detail, before, after, ip)
-
+        self._write_db_log(
+            action_time, username, user_role,
+            "UPDATE", employee_id, detail,
+            before, after, ip
+        )
